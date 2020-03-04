cce24d26a93ff50b28430b0d8bb5032f63a7953d
Silecarf-SE <silecarf@gmail.com>
2020-03-04 01:25:11 +0000

    type

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-print("hello");
+print("hello")
 
-print("this is test python repo");
+print("this is test python repo")
+
+x=1
+y=2
+print(x+y)
+
+x = 2
+y = 2
+z = 1.2
+
+print(x + y)
+print(x % y)
+print(x**y)
+
+a="hello"
+b='bye'
+print(a+' '+b)
+z= """ 안녕하세요 공부중입니다. """
+print (z)
+
+print("난 "+str(28)+"살이야") # 캐스팅은 여기도 있다.
+x=4
+y="4"
+print(str(x)+y)
+print(x+int(y))
+x=True;
+y=False;
+if not y and y or x:
+  print("true")
+else :
+  print("false")
+x=3
+
+if x>5:
+  print("x>=3")
+elif x==3:
+  print("x=3")
+else:
+  print("x<2")
